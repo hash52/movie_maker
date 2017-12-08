fc77cdd8f392e9657f673656297cb20304c770eb
ULM Co <ULM@Takayuki-no-iMac.local>
2017-12-08 11:13:14 +0900

    Get page data

--- app/controllers/webpage_data_controller.rb
+++ app/controllers/webpage_data_controller.rb
@@ -1,4 +1,7 @@
 class WebpageDataController < ApplicationController
   def test
+    agent = Mechanize.new
+    page = agent.get('http://himasoku.com/archives/52018897.html')
+    elements = page.search('title')
   end
 end
